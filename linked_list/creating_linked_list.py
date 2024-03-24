1eabc39e11a775f2a25e645be289fb5d98f2b48d
Divya Devadas <divya9devadas@gmail.com>
2024-03-24 13:15:07 +0000

    Linked list file

---
+++ linked_list/creating_linked_list.py
@@ -0,0 +1,17 @@
+class LinkedList:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = next
+
+
+node1 = LinkedList(100)
+node2 = LinkedList(200)
+
+node1.next = node2
+head = node1
+
+current = head
+while current is not None:
+    print(current.data)
+    current = current.next
