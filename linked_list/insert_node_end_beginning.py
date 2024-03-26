db93b96baee26c44dba3e4b6c653c636943f08ce
Divya Devadas <divya9devadas@gmail.com>
2024-03-26 20:36:03 +0000

    updading linked list

---
+++ linked_list/insert_node_end_beginning.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = Node(0)
+
+    def add_node(self, node:Node):
+        current = self.head
+
+        while current.next is not None:
+            current = current.next
+        current.next = node
+
+    def add_to_beginning(self, node:Node):
+        node.next = self.head.next
+        self.head.next = node
+
+    def print_linked_list(self):
+        current = self.head
+        while current is not None:
+            print (current.data)
+            current = current.next
+
