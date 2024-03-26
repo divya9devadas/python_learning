e5863e5a0bc61153e3ac6c1c6c3257f23f27639e
Divya Devadas <divya9devadas@gmail.com>
2024-03-26 21:00:57 +0000

    Updating search_element file

--- linked_list/search_element.py
+++ linked_list/search_element.py
@@ -1,3 +1,6 @@
+class LinkedList:
+    def __init__(self):
+        self.head = Node(0)
 def search(self, target_node:Node):
     current = self.head
     while current is not None
@@ -7,3 +10,7 @@ def search(self, target_node:Node):
         current = current.next
     print("Not found")
     return
+
+if __name__ == "__main__":
+    target_node = Node(50)
+
