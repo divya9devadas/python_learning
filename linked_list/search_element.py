be686b78f009fb57a9b283c150dcdf1a620fe051
Divya Devadas <divya9devadas@gmail.com>
2024-03-26 20:56:21 +0000

    Updating search_element file

--- linked_list/search_element.py
+++ linked_list/search_element.py
@@ -1,3 +1,6 @@
 def search(self, target_node:Node):
     current = self.head
     while current is not None
+        if current.data == target_node.data:
+            print("Found")
+            return
