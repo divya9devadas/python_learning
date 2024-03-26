8d31304560a6f3ab6416287c1e356b8c2cda4c2c
Divya Devadas <divya9devadas@gmail.com>
2024-03-26 20:50:38 +0000

    updading linked list

--- linked_list/insert_node_end_beginning.py
+++ linked_list/insert_node_end_beginning.py
@@ -3,31 +3,42 @@ class Node:
         self.data = data
         self.next = None
 
+
 class LinkedList:
     def __init__(self):
         self.head = Node(0)
 
-    def add_node(self, node:Node):
+    def add_node(self, node: Node):
         current = self.head
 
         while current.next is not None:
             current = current.next
         current.next = node
 
-    def add_to_beginning(self, node:Node):
+    def add_to_beginning(self, node: Node):
         node.next = self.head.next
         self.head.next = node
 
     def print_linked_list(self):
         current = self.head
         while current is not None:
-            print (current.data)
+            print(current.data)
             current = current.next
 
     def print_size(self):
         current = self.head
         count = 0
         while current is not None:
-            count = count+1
+            count = count + 1
             current = current.next
-            return count-1
+            return count - 1
+
+
+if __name__ == "__main__":
+    for number in [25, 50, 100, 150, 200, 250, 300]:
+        node = Node(number)
+        linked_list = LinkedList()
+        linked_list.add_node(node)
+
+    linked_list.print_linked_list()
+    print(linked_list.print_size)
