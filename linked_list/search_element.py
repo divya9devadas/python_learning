9741535c7ab310c47d657fb3a14e725e8698aca2
Divya Devadas <divya9devadas@gmail.com>
2024-03-27 13:15:54 +0000

    Updating search element file

--- linked_list/search_element.py
+++ linked_list/search_element.py
@@ -1,3 +1,9 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
 class LinkedList:
     def __init__(self):
         self.head = Node(0)
@@ -9,16 +15,22 @@ class LinkedList:
             current = current.next
         current.next = node
 
-def search(self, target_node:Node):
-    current = self.head
-    while current is not None
-        if current.data == target_node.data:
-            print("Found")
-            return
-        current = current.next
-    print("Not found")
-    return
+    def search(self, target_node: Node):
+        current = self.head
+        while current is not None:
+            if current.data == target_node.data:
+                print("Element found")
+                return
+            current = current.next
+        print("Element not found")
+        return
+
 
 if __name__ == "__main__":
-    target_node = Node(50)
+    linked_list = LinkedList()
+    nodes = [Node(num) for num in [25, 50, 100, 150, 200, 250, 300]]
+    for node in nodes:
+        linked_list.add_node(node)
 
+    target_node = Node(10)
+    linked_list.search(target_node)
