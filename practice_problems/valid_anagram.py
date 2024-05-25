8ef5d7b876b3e4b35337cc17b5f21ffd1bfedf55
Divya Devadas <divya9devadas@gmail.com>
2024-05-25 08:54:07 +0100

    Udating valid_anagram

--- practice_problems/valid_anagram.py
+++ practice_problems/valid_anagram.py
@@ -1,3 +1,8 @@
 def isAnagram(k, l):
     k_counter = {}
     l_counter = {}
+
+    for letter in k:
+        if letter in k_counter:
+            k_counter[letter] = 0
+        k_counter[letter] = k_counter[letter] + 1
