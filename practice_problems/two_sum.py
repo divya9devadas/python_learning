78082756e52f05fc858961dca5477b120782b223
Divya Devadas <divya9devadas@gmail.com>
2024-05-25 09:07:05 +0100

    Updating two_sum

--- practice_problems/two_sum.py
+++ practice_problems/two_sum.py
@@ -3,3 +3,6 @@ def twoSum(nums, target):
 
     for ind in range(len(nums)):
         complement = target - nums[ind]
+        if complement in temp_dict:
+            return temp_dict[complement], ind
+        temp_dict[nums[ind]] = ind
