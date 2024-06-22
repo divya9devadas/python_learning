6374645703a47c97a798720341f00fce7f2ceedc
Divya Devadas <divya9devadas@gmail.com>
2024-06-22 12:21:08 +0100

    Adding merging two lists

---
+++ python_practice_problems/python_basics_excercise/merge_lists.py
@@ -0,0 +1,10 @@
+# Write a function to merge two lists.
+
+def merge_lists(list1, list2):
+    return list1 + list2
+
+
+list1 = "Write a function to merge two lists."
+list2 = "English is often considered tattarrattat"
+
+print(merge_lists(list1, list2))
