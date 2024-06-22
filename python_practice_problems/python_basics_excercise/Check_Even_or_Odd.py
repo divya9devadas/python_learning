13d9745487dd63ba3666759f0a8786a839ffcc5c
Divya Devadas <divya9devadas@gmail.com>
2024-06-22 11:23:20 +0100

    Adding checking if the number is even or odd

---
+++ python_practice_problems/python_basics_excercise/Check_Even_or_Odd.py
@@ -0,0 +1,8 @@
+# Write a function that checks if a number is even or odd.
+
+def odd_or_even(num):
+    return "Number is EVEN" if num % 2 == 0 else "Number is ODD"
+
+
+num = 65
+print(odd_or_even(num))
---
+++ python_practice_problems/python_basics_excercise/sum_of_two_numbers.py
@@ -0,0 +1,9 @@
+# Write a function that takes two numbers and returns their sum.
+
+def sum_of_nums(num1, num2):
+    return num1 + num2
+
+
+num1 = 5
+num2 = 8
+print(sum_of_nums(num1, num2))
