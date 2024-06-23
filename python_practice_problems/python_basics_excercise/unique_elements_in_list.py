f64d42edf572ba1645925c4aac957c9753e87586
Divya Devadas <divya9devadas@gmail.com>
2024-06-23 20:06:44 +0100

    updating unique elements in a list file

--- python_practice_problems/python_basics_excercise/unique_elements_in_list.py
+++ python_practice_problems/python_basics_excercise/unique_elements_in_list.py
@@ -5,3 +5,11 @@
 def unique_elements(list):
     seen = set()
     unique_list = []
+    for i in list:
+        if i not in seen:
+            seen.add(i)
+            unique_list.append(i)
+    return unique_list
+
+num = [1,3,4,2,6,3,5,1,7,2,8,3,9,4,9,6]
+print(f'List with unique elements')
