c8790efdfef93c7ccc03bc33ea4a3b0f3e4d03ed
Divya Devadas <divya9devadas@gmail.com>
2024-06-24 19:50:01 +0100

    updating FIZZBUZZ file

--- python_practice_problems/python_basics_excercise/fizzBuzz.py
+++ python_practice_problems/python_basics_excercise/fizzBuzz.py
@@ -6,8 +6,13 @@
 def fizz_buzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            return "FizzBuzz"
+            print("FizzBuzz")
         elif i % 3 == 0:
             return "Fizz"
         elif i % 5 == 0:
             return "Buzz"
+        else:
+            return i
+
+
+fizz_buzz()
