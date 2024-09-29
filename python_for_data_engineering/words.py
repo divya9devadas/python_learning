245e4ddd696aaa0072a8315c649dee488ccddc83
Divya Devadas <divya9devadas@gmail.com>
2024-09-29 20:40:08 +0100

    Updating 'python_for_data_engineering/words' file by adding basic test case

--- python_for_data_engineering/words.py
+++ python_for_data_engineering/words.py
@@ -1,11 +1,23 @@
 # count the average length of words in a set
 
+import unittest
 def avg_word_len(words: set) -> float:
     total_len = sum(len(word) for word in words)
-    return total_len/len(words)
+    return total_len / len(words)
 
-if __name__=='__main__':
+
+class TestAvgWordLen(unittest.TestCase):
+
+    def test_basic_case(self):
+        words_set = {"elephant", "tiger", "zebra", "giraffe", "monkey"}
+        expected_result = (8 + 5 + 5 + 7 + 6) / 5  # manually calculated
+        self.assertAlmostEqual(avg_word_len(words_set), expected_result)
+
+
+if __name__ == '__main__':
     words_set = {"elephant", "tiger", "zebra", "giraffe", "monkey"}
 
     solution = avg_word_len(words=words_set)
     print(solution)
+
+    unittest.main()
