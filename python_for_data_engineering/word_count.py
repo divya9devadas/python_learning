285e532db3cd461f16bc8d0f48cc32310ff55877
Divya Devadas <divya9devadas@gmail.com>
2024-09-29 20:24:20 +0100

    Updating 'python_for_data_engineering/word _count' file by adding basic test case

--- python_for_data_engineering/word_count.py
+++ python_for_data_engineering/word_count.py
@@ -2,6 +2,7 @@
 
 from collections import defaultdict
 from typing import Dict
+import unittest
 
 
 def solution(string: str) -> Dict[str, int]:
@@ -11,10 +12,38 @@ def solution(string: str) -> Dict[str, int]:
         word_count[word] += 1
     return word_count
 
+class TestSolution(unittest.TestCase):
+    def test_basic_case(self):
+        input_string = """data engineering is a good skill to have
+        # Data is the new oil
+        # Data engineering can be massive"""
+
+        expected_output = {
+        'data': 3,
+        'engineering': 2,
+        'is': 2,
+        'a': 1,
+        'good': 1,
+        'skill': 1,
+        'to': 1,
+        'have': 1,
+        '#': 2,
+        'the': 1,
+        'new': 1,
+        'oil': 1,
+        'can': 1,
+        'be': 1,
+        'massive': 1
+        }
+        self.assertEqual(solution(input_string), expected_output)
+
 
 if __name__ == "__main__":
     input_string = """data engineering is a good skill to have
 # Data is the new oil
 # Data engineering can be massive"""
+
     count = solution(string=input_string)
     print(count)
+
+    unittest.main()
